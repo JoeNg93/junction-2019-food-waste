183df4e446e30511fc37421ff4de5aaaf984fe49
khangnguyen <khang.nguyen@giosg.com>
2019-11-15 20:49:30 +0200

    Routing and init structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import './App.css';
+import QRSection from './components/QRSection/index';
+import RegriSection from './components/RefriSection/index';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route path="/" exact>
+            <RegriSection/>
+          </Route>
+          <Route path="/qr_scan" exact>
+            <QRSection/>
+          </Route>
+        </Switch>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/qr_scan">QR scan</Link>
+            </li>
+          </ul>
+        </nav>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/components/RefriSection/index.js
@@ -0,0 +1,12 @@
+import React,  { useState, useEffect } from 'react';
+
+
+const RegriSection = () => {
+  return (
+    <div>
+      <h1>This is refri section</h1>
+    </div>
+  )
+}
+
+export default RegriSection;
