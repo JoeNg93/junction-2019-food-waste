1646588fe2ad8267dbdb9b702386d60b5bf98d68
khangnguyen <khang.nguyen@giosg.com>
2019-11-15 21:53:10 +0200

    Camera icon open qr route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,35 +2,30 @@ import React from 'react';
 import QRSection from './components/QRSection/index';
 import RegriSection from './components/RefriSection/index';
 import {
-  BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory
 } from "react-router-dom";
+import { Button, Icon } from 'antd';
+import 'antd/dist/antd.css';
+
 
 function App() {
+  let history = useHistory();
   return (
     <div className="App">
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            <RegriSection/>
-          </Route>
-          <Route path="/qr_scan" exact>
-            <QRSection/>
-          </Route>
-        </Switch>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/qr_scan">QR scan</Link>
-            </li>
-          </ul>
-        </nav>
-      </Router>
+      <Switch>
+        <Route path="/" exact>
+          <RegriSection/>
+        </Route>
+        <Route path="/qr_scan" exact>
+          <QRSection/>
+        </Route>
+      </Switch>
+      <nav>
+        <Icon type="camera" onClick={() => history.push("/qr_scan")}/>
+      </nav>
     </div>
   );
 }
