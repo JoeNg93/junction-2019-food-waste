4d3783a61a3f35eb78123c083c4d9fa6ead0afd3
Joe Nguyen <ntuandung93@gmail.com>
2019-11-16 11:04:51 +0200

    Update purchase history API
    Signed-off-by: Joe Nguyen <ntuandung93@gmail.com>

--- back-end/src/controllers/PurchaseHistoryController.ts
+++ back-end/src/controllers/PurchaseHistoryController.ts
@@ -9,18 +9,25 @@ router.get('/:productId', async (req, res) => {
   try {
     const { productId } = req.params;
     const { product_history } = await db.getData();
-    const purchase_history = product_history[productId]
-        .reduce((obj, purchase) => {
-            const purchaseDay = purchase.purchase_date.split('/')[0];
-            if (obj[purchaseDay]) {
-                obj[purchaseDay].quantity += purchase.quantity;
-                return obj;
-            }
-            obj[purchaseDay] = purchase;
-            obj[purchaseDay].purchase_date = purchaseDay;
-            return obj;
-        }, {} as PurchaseHistory);
-    return res.status(200).send(Object.values(purchase_history));
+    const purchase_history = Object.values(
+      product_history[productId].reduce((obj, purchase) => {
+        const purchaseDay = purchase.purchase_date.split('/')[0];
+        if (obj[purchaseDay]) {
+          obj[purchaseDay].quantity += purchase.quantity;
+          return obj;
+        }
+        obj[purchaseDay] = purchase;
+        obj[purchaseDay].purchase_date = purchaseDay;
+        return obj;
+      }, {} as PurchaseHistory)
+    );
+    return res.status(200).send({
+      productName: purchase_history[0].name,
+      purchaseHistory: purchase_history.map(ph => ({
+        date: ph.purchase_date,
+        qty: ph.quantity,
+      })),
+    });
   } catch (e) {
     return res.status(500).send(e);
   }
