c5e6d5cbfb36d5267d4bc3c0dbd9092ab83d455f
Thanh Dang <dang.np.thanh@gmail.com>
2019-11-16 09:45:55 +0200

    Add styling for calendar header

--- frontend/src/components/FrequencyCalendar/CalendarHeader.js
+++ frontend/src/components/FrequencyCalendar/CalendarHeader.js
@@ -1,44 +1,76 @@
 import React, { useState } from 'react';
 import { Row, Icon } from 'antd';
+import { StyleSheet, css } from 'aphrodite';
 
-const CalendarHeader = ({ value, type, onChange, focusToday, renderPurchaseMessage }) => {
-    const current = value.clone(),
-        localeData = value.localeData();
+const CalendarHeader = ({
+  value,
+  type,
+  onChange,
+  focusToday,
+  renderPurchaseMessage
+}) => {
+  const current = value.clone(),
+    localeData = value.localeData();
 
-    const [month, setMonth] = useState(value.month());
-    const [year, setYear] = useState(value.year());
+  const [month, setMonth] = useState(value.month());
+  const [year, setYear] = useState(value.year());
 
-    const onPrev = () => {
-            const newValue = value.clone();
-            newValue.subtract(1, 'month');
-            setMonth(newValue.month());
-            setYear(newValue.year());
-            onChange(newValue);
-        },
-        onNext = () => {
-            const newValue = value.clone();
-            newValue.add(1, 'month');
-            setMonth(newValue.month());
-            setYear(newValue.year());
-            onChange(newValue);
-        };
+  const onPrev = () => {
+      const newValue = value.clone();
+      newValue.subtract(1, 'month');
+      setMonth(newValue.month());
+      setYear(newValue.year());
+      onChange(newValue);
+    },
+    onNext = () => {
+      const newValue = value.clone();
+      newValue.add(1, 'month');
+      setMonth(newValue.month());
+      setYear(newValue.year());
+      onChange(newValue);
+    };
 
-    return (
-        <div style={{ padding: 10 }}>
-            <Row>
-                <span>{renderPurchaseMessage(value)}</span>
-            </Row>
-            <Row type="flex" justify="space-between">
-                <Icon type="left" onClick={onPrev} />
-                <span>{month + 1}</span>
-                <span>{year}</span>
-                <Icon type="right" onClick={onNext} />
-            </Row>
-            <Row>
-                <span onClick={focusToday}>Today</span>
-            </Row>
-        </div>
-    );
+  return (
+    <div className={css(styles.headerWrapper)}>
+      <Row type="flex" align="center">
+        <span className={css(styles.purchaseMessage)}>
+          {renderPurchaseMessage(value)}
+        </span>
+      </Row>
+      <Row className={css(styles.navRow)} type="flex" justify="space-between">
+        <Icon type="left" onClick={onPrev} />
+        <span>{month + 1}</span>
+        <span>{year}</span>
+        <Icon type="right" onClick={onNext} />
+      </Row>
+      <Row className={css(styles.todayRow)} type="flex" align="center">
+        <span className={css(styles.todayHelper)} onClick={focusToday}>
+          Today
+        </span>
+      </Row>
+    </div>
+  );
 };
+const styles = StyleSheet.create({
+  headerWrapper: {
+    paddingTop: 16
+  },
+  navRow: {
+    margin: 16
+  },
+  purchaseMessage: {
+    fontSize: 20,
+    color: '#1890ff'
+  },
+  todayHelper: {
+    textDecoration: 'underline',
+    cursor: 'pointer',
+    color: 'white'
+  },
+  todayRow: {
+    background: '#1890ff',
+    padding: 4
+  }
+});
 
-export default CalendarHeader
+export default CalendarHeader;
