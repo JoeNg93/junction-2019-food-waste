274d4640f6d636ac1a91b6759008506a1ec55af9
Joe Nguyen <ntuandung93@gmail.com>
2019-11-16 20:28:31 +0200

    Add navigation bar
    Signed-off-by: Joe Nguyen <ntuandung93@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,162 @@
-import React from 'react';
+import React, { useState } from 'react';
 import QRSection from './components/QRSection/index';
 import RefriSection from './components/RefriSection/index';
 import { Switch, Route } from 'react-router-dom';
 import 'antd/dist/antd.css';
+import { useHistory } from 'react-router-dom';
+import { TabBar } from 'antd-mobile';
+import style from './constants/styleVariables';
+import { TiCamera } from 'react-icons/ti';
+import { StyleSheet, css } from 'aphrodite';
+
+const NavBar = () => {
+  const [selectedTab, setSelectedTab] = useState('redTab');
+  const history = useHistory();
+
+  return (
+    <div
+      style={{ position: 'fixed', height: '300px', width: '100%', bottom: 0 }}
+    >
+      <TabBar
+        unselectedTintColor="#949494"
+        tintColor="#33A3F4"
+        barTintColor="white"
+        hidden={window.location.pathname === '/qr_scan'}
+      >
+        <TabBar.Item
+          title="Fridge"
+          key="Fridge"
+          icon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          selectedIcon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          selected={selectedTab === 'blueTab'}
+          badge={1}
+          onPress={() => {
+            setSelectedTab('blueTab');
+            history.push('/');
+          }}
+        ></TabBar.Item>
+        <TabBar.Item
+          icon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          selectedIcon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          title="Recipes"
+          key="Recipes"
+          badge={'new'}
+          selected={selectedTab === 'redTab'}
+          onPress={() => {
+            setSelectedTab('redTab');
+            history.push('/recipes');
+          }}
+        ></TabBar.Item>
+        <TabBar.Item
+          icon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          selectedIcon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          title="Scan History"
+          key="Scan History"
+          dot
+          selected={selectedTab === 'greenTab'}
+          onPress={() => {
+            setSelectedTab('greenTab');
+            history.push('/scan-history');
+          }}
+        ></TabBar.Item>
+        <TabBar.Item
+          icon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          selectedIcon={
+            <div
+              style={{
+                width: '22px',
+                height: '22px',
+                background:
+                  'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat'
+              }}
+            />
+          }
+          title="User Settings"
+          key="User Settings"
+          dot
+          selected={false}
+        ></TabBar.Item>
+      </TabBar>
+
+      <div
+        className={css(styles.cameraNavWrapper)}
+        onClick={() => history.push('/qr_scan')}
+      >
+        <TiCamera
+          style={{
+            fontSize: 45,
+            color: 'white',
+            position: 'relative',
+            top: -4
+          }}
+        />
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
@@ -15,8 +169,28 @@ function App() {
           <QRSection />
         </Route>
       </Switch>
+      <NavBar />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  cameraNavWrapper: {
+    position: 'fixed',
+    left: '50%',
+    marginLeft: '-40px',
+    bottom: -12,
+    textAlign: 'center',
+    width: 80,
+    height: 80,
+    borderRadius: '100%',
+    background: style.primaryColor,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    cursor: 'pointer',
+    zIndex: 9999
+  }
+});
 
 export default App;
